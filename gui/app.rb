2b1c1ca6d966ffbf451dec6e2eafce283e2be1f7
Lukas_Skywalker <lukas.diener@hotmail.com>
2017-08-17 15:18:39 +0200

    add drawing

--- gui/app.rb
+++ gui/app.rb
@@ -1,37 +1,48 @@
 require 'gosu'
+require_relative 'path'
+require_relative 'point'
 
 class Tutorial < Gosu::Window
   def initialize
     super 1024, 768
     self.caption = "Hack GUI"
+    @drawing = false
+    @paths = []
   end
   
   def update
-    # ...
+    if @drawing
+      @paths.last.points << Point.new(self.mouse_x, self.mouse_y)
+    end
   end
   
   def draw
     Gosu.draw_rect(10, 10, 100, 100, Gosu::Color::GREEN)
+
+    @paths.each do |path|
+      path.draw
+    end
   end
 
   def button_down(id)
-    puts "X: #{self.mouse_x}; Y: #{self.mouse_y}"
     case id
     when Gosu::MsLeft
-      puts "left"
-    when Gosu::MsRight
-      puts "right"
+      @drawing = true
+      @paths << Path.new
+    when Gosu::KbEscape
+      self.close
     end
   end
 
   def button_up(id)
-    puts "X: #{self.mouse_x}; Y: #{self.mouse_y}"
     case id
     when Gosu::MsLeft
-      puts "left"
-    when Gosu::MsRight
-      puts "right"
+      @drawing = false
     end
+  end
+
+  def needs_cursor?
+    true
   end
 end
 
